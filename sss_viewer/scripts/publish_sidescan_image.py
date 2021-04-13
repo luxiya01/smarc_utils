361d11dae55a6a177ba12a87b22e66d416e533b1
Li Ling <ling@seahorse-ling.mbbsflsjfpsuthw3a5qipow5kd.ax.internal.cloudapp.net>
2021-04-13 16:06:38 +0000

    Publish sidescan raw image (needed to publish the compressed version)

--- sss_viewer/scripts/publish_sidescan_image.py
+++ sss_viewer/scripts/publish_sidescan_image.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import cv2
 import rospy
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image, CompressedImage
 from smarc_msgs.msg import Sidescan
 
 class SidescanPublisher:
     """Subscribe to /vehicle/payload/sidescan message and publish the
-    corresponding waterfall image to /vehicle/payload/sidescan/CompressedImage"""
+    corresponding waterfall image to /vehicle/payload/sidescan/image/
+    and /vehicle/payload/sidescan/image/compressed"""
 
     def __init__(self, width_in=1000, width_out=256, height=1000):
         self.bridge = CvBridge()
@@ -18,7 +19,8 @@ class SidescanPublisher:
         self.img = np.zeros((self.height, 2 * self.width_in), dtype=np.ubyte)
         self.sidescan_sub = rospy.Subscriber("/sam/payload/sidescan", Sidescan,
                 self.callback)
-        self.sidescan_image_pub = rospy.Publisher("/sam/payload/sidescan/CompressedImage", CompressedImage)
+        self.sidescan_image_pub = rospy.Publisher("/sam/payload/sidescan/image", Image, queue_size=2)
+        self.sidescan_image_compressed_pub = rospy.Publisher("/sam/payload/sidescan/image/compressed", CompressedImage, queue_size=2)
 
     def callback(self, msg):
         port = np.array(bytearray(msg.port_channel), dtype=np.ubyte)
@@ -32,11 +34,12 @@ class SidescanPublisher:
         resized_img = cv2.resize(self.img, (2*self.width_out, self.height),
                 interpolation=cv2.INTER_AREA)
         try:
-            imgmsg = self.bridge.cv2_to_compressed_imgmsg(resized_img,
-                    "passthrough")
+            imgmsg = self.bridge.cv2_to_imgmsg(resized_img)
             self.sidescan_image_pub.publish(imgmsg)
+            compressed_imgmsg = self.bridge.cv2_to_compressed_imgmsg(resized_img)
+            self.sidescan_image_compressed_pub.publish(compressed_imgmsg)
         except CvBridgeError as e:
-            print(f"Converting numpy image to imgmsg: {e}")
+            print(f"Error converting numpy image to imgmsg: {e}")
 
 
 def main():
